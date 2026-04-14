17c646652643217ab5d8cb7ff750f8a543c5e964
johnrlabs-creator <johnrlabs@gmail.com>
2026-04-14 14:25:02 +0800

    Add environment file and config for path shortcut

--- src/app/core/services/vehicle-api-service.ts
+++ src/app/core/services/vehicle-api-service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Vehicle } from '../interfaces/vehicle.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleApiService {
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
